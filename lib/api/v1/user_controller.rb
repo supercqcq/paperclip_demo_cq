f9109e0351f3c836e682b868396d24f74bb1b195
chenqingyo <chenqingyo@gmail.com>
2014-07-09 15:45:17 +0800

    post 方式调用/api/v1/user/create

--- lib/api/v1/user_controller.rb
+++ lib/api/v1/user_controller.rb
@@ -1,10 +1,30 @@
 module Api::V1::UserController
     def self.registered(app)
+
+        app.helpers Helpers
+
+        app.post '/user/create' do
+            Rails.logger.info(params[:user])
+            @user = User.new(params[:user])
+            if @user.save
+                Rails.logger.info("logger save OK")
+            else
+                Rails.logger.info("logger save ERROR")
+            end
+            render_json(:id => @user[:id])
+        end
+
         # /api/v1/hello
         # http://localhost:4000/api/v1/hello?name=superCQ&id=20
         # 显示结果: Hello superCQ, id=20
         app.get '/hello' do
             "Hello #{params[:name]}, id=#{params[:id]}"
+        end
+    end
+
+    module Helpers
+        def render_json(json={})
+            JSON.generate({:status => 0, :msg => "success."}.merge(json))
         end
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
     describe "GET /users" do
         it "works! (now write some real specs)" do
-        # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-        # get users_path
-        # response.status.should be(200)
+            it { should validate_presence_of :name }
+            it { should_not validate_attachment_presence :avatar }
+            it { should validate_attachment_content_type(:avatar).allowing('image/png', 'image/jpg', 'image/jpeg').rejecting('text/plain', 'text/html')}
+            it { should validate_attachment_size(:avatar).less_than(5.megabytes) }
         end
     end
 end
